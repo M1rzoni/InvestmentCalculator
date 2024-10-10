95d385c082e02af866ee593e9c657744f2c935a9
Mirza <mirza.sabanovic@ipi-akademija.ba>
2024-10-10 23:11:14 +0200

    design done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
