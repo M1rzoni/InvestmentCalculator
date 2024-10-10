b57142926551f49fed65f487eeedb214fb7991fb
Mirza <mirza.sabanovic@ipi-akademija.ba>
2024-10-10 23:48:00 +0200

    handling inputs with ngModel and creating ts file for calculation

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.css',
 })
 export class CalculatorComponent {
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedRetrun = '5';
+  duration = '10';
 
+  onSubmit() {
+    console.log('Submited');
+    console.log(this.annualInvestment);
+    console.log(this.initialInvestment);
+    console.log(this.expectedRetrun);
+    console.log(this.duration);
+  }
 }
